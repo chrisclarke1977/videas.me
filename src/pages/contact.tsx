2ecf3ff8c588de16cdc739bc31ca3a13e48654c4
Chris Clarke <chrisclarke1977@gmail.com>
2021-11-19 13:41:09 +0000

    adjust contact us

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,28 +10,60 @@ const Contact = () => {
     <div className="contact">
       <p>Please get in touch we love to talk about what we are building</p>
       <p hidden>
-        <label>
+        <label className="block">
           Don’t fill this out: <input name="bot-field" />
         </label>
       </p>
-      <form
-        className="container"
-        method="POST"
-        name="contact-form"
-        action="/?success=true"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <label htmlFor="name">Name *</label>
-        <input id="name" name="name" required type="text" />
-        <label htmlFor="company">Company *</label>
-        <input id="company" name="company" required type="text" />
-        <label htmlFor="email">E-mail Address *</label>
-        <input id="email" type="email" name="email" required />
-        <label htmlFor="message">Message *</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
+        <div className="grid grid-cols-1 gap-6">
+          <form
+            className="container"
+            method="POST"
+            name="contact"
+            action="/?success=true"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <label className="block border-collapse" htmlFor="name">
+              <span className="text-gray-700">Name *</span>
+              <input
+                className="form-input px-4 py-3 rounded-full"
+                id="name"
+                name="name"
+                required
+                type="text"
+              />
+            </label>
+            <label className="block" htmlFor="company">
+              <span className="text-gray-700">Company *</span>
+              <input
+                className="form-input px-4 py-3 rounded-full"
+                id="company"
+                name="company"
+                required
+                type="text"
+              />
+            </label>
+            <label className="block" htmlFor="email">
+              <span className="text-gray-700">E-mail Address *</span>
+              <input
+                className="form-input px-4 py-3 rounded-full"
+                id="email"
+                type="email"
+                name="email"
+                required
+              />
+            </label>
+            <label className="block" htmlFor="message">
+              <span className="text-gray-700">Message *</span>
+              <textarea id="message" name="message" required></textarea>
+            </label>
+            <button className="bg-blend-saturation" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
@@ -46,7 +78,7 @@ const ContactPage = () => {
     <React.Fragment>
       <p>
         Thank you for submitting this form. Someone should get back to you
-        within 24-48 hours.
+        within your lifetime.
       </p>
 
       <button
