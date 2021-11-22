90a5edf743c5818818a5b8113f0b7cd96ea097ab
Chris Clarke <chrisclarke1977@gmail.com>
2021-11-22 11:23:05 +0000

    move blog content

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+
+const blogPostsFolder = './content/blogPosts';
+
+const getPathsForPosts = () => {
+  return fs
+    .readdirSync(blogPostsFolder)
+    .map((blogName) => {
+      const trimmedName = blogName.substring(0, blogName.length - 3);
+      return {
+        [`/blog/post/${trimmedName}`]: {
+          page: '/blog/post/[slug]',
+          query: {
+            slug: trimmedName,
+          },
+        },
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {});
+};
+
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -11,4 +34,17 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  webpack: (configuration) => {
+    configuration.module.rules.push({
+      test: /\.md$/,
+      use: 'frontmatter-markdown-loader',
+    });
+    return configuration;
+  },
+  async exportPathMap(defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      ...getPathsForPosts(),
+    };
+  },
 });
